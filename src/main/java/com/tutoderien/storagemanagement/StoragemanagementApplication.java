7f0d9a780a443f28d24ff967c3f901a8bc1f179d
sondeck3000 <sondeckdo@gmail.com>
2020-11-27 09:06:44 +0100

    CONNEXION À LA BASE DE DONNÉES

---
+++ src/main/java/com/tutoderien/storagemanagement/StoragemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.tutoderien.storagemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoragemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutoderien/storagemanagement/StoragemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoderien.storagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoragemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
